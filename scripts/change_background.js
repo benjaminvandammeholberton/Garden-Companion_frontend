14bec4b34674191c9a04b12be2e0c77f3870c409
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-11-10 17:06:20 +0100

    Improve design of background

--- scripts/change_background.js
+++ scripts/change_background.js
@@ -1,20 +1,20 @@
-// Add an event listener to the background selection dropdown
-const backgroundSelect = document.getElementById("background-select");
+// // Add an event listener to the background selection dropdown
+// const backgroundSelect = document.getElementById("background-select");
 
-// Load the user's background choice from local storage (if available)
-const selectedBackground = localStorage.getItem("selectedBackground");
-if (selectedBackground) {
-    document.body.style.backgroundImage = `url(${selectedBackground})`;
-    backgroundSelect.value = selectedBackground; // Update the selected option in the dropdown
-}
+// // Load the user's background choice from local storage (if available)
+// const selectedBackground = localStorage.getItem("selectedBackground");
+// if (selectedBackground) {
+//     document.body.style.backgroundImage = `url(${selectedBackground})`;
+//     backgroundSelect.value = selectedBackground; // Update the selected option in the dropdown
+// }
 
-backgroundSelect.addEventListener("change", () => {
-    // Get the selected background value
-    const selectedBackground = backgroundSelect.value;
+// backgroundSelect.addEventListener("change", () => {
+//     // Get the selected background value
+//     const selectedBackground = backgroundSelect.value;
 
-    // Update the background-image of the body based on the user's selection
-    document.body.style.backgroundImage = `url(${selectedBackground})`;
+//     // Update the background-image of the body based on the user's selection
+//     document.body.style.backgroundImage = `url(${selectedBackground})`;
 
-    // Store the selected background in local storage
-    localStorage.setItem("selectedBackground", selectedBackground);
-});
+//     // Store the selected background in local storage
+//     localStorage.setItem("selectedBackground", selectedBackground);
+// });
