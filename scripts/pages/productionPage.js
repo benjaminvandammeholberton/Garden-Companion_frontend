a06ab9b51af47bbb56d5225bbca27e04c7b48005
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-12-08 09:53:25 +0100

    Add feature: production page

--- scripts/pages/productionPage.js
+++ scripts/pages/productionPage.js
@@ -1,3 +1,125 @@
+import * as areaApi from '../api/areaApi.js';
+import * as vegetableManagerApi from '../api/vegetableManagerApi.js';
+
+const productionPage = document.getElementById('production_page');
+productionPage.addEventListener('click', async () => {
+  const productionContent = document.getElementById('productionContent');
+  productionContent.innerHTML = '';
+  const vegetablesData = await vegetableManagerApi.getVegetableManager();
+  const areasData = await areaApi.getAreas();
+
+  areasData.forEach((area) => {
+    const areaName = document.createElement('h3');
+    areaName.classList.add('production-container__content__subtitle');
+    areaName.textContent = area.name;
+
+    const tableProduction = document.createElement('table');
+    tableProduction.classList.add('table-production');
+    tableProduction.innerHTML = headerTable;
+
+    productionContent.appendChild(areaName);
+    productionContent.appendChild(tableProduction);
+
+    const vegetablesInArea = vegetablesData.filter((vegetable) => {
+      return vegetable.area.area_id === area.area_id;
+    });
+
+    vegetablesInArea.forEach((vegetable) => {
+      const tableRow = document.createElement('tr');
+      let startMonth;
+      let startDay;
+      let startCell;
+      let endMonth;
+      let endDay;
+      let endCell;
+      let harvestCell;
+
+      const currentDate = new Date();
+
+      if (vegetable.harvest_date) {
+        const harvestDate = new Date(vegetable.harvest_date);
+        const harvestMonth = harvestDate.getMonth();
+        const harvestDay = harvestDate.getDate();
+        harvestCell = parseInt((harvestMonth + 1) * 4 + harvestDay / 7 - 1);
+      }
+
+      if (vegetable.sowing_date) {
+        const sowingDate = new Date(vegetable.sowing_date);
+        startMonth = sowingDate.getMonth();
+        startDay = sowingDate.getDate();
+      }
+
+      if (vegetable.planting_date) {
+        const plantingDate = new Date(vegetable.planting_date);
+        startMonth = plantingDate.getMonth();
+        startDay = plantingDate.getDate();
+      }
+      startCell = parseInt((startMonth + 1) * 4 + startDay / 7 - 1);
+
+      if (vegetable.remove_date) {
+        const removeDate = new Date(vegetable.remove_date);
+        endMonth = removeDate.getMonth();
+        endDay = removeDate.getDate();
+        endCell = parseInt((endMonth + 1) * 4 + endDay / 7 - 1);
+      } else {
+        endMonth = currentDate.getMonth();
+        endDay = currentDate.getDate();
+        endCell = parseInt((endMonth + 1) * 4 + endDay / 7 - 1);
+      }
+
+      for (let i = 0; i <= 51; i++) {
+        const tableCell = document.createElement('td');
+        if ((i > 0 && i < 3) || i === 51) {
+          tableCell.setAttribute('colspan', '4');
+        }
+        if (i === 0) {
+          tableCell.textContent = '';
+          tableCell.style.backgroundImage = `url(./styles/assets/vegetable_icons/${vegetable.name.replace(
+            /\s/g,
+            ''
+          )}.png)`;
+
+          tableCell.classList.add('table-production__vegetable-icon');
+        }
+        if (i === 1) {
+          tableCell.textContent = vegetable.name;
+        }
+        if (i === 2) {
+          tableCell.textContent = vegetable.quantity;
+        }
+        if (i === 51) {
+          tableCell.textContent = vegetable.harvest_quantity;
+        }
+
+        if (i >= startCell && i <= endCell) {
+          const timePeriod = document.createElement('div');
+          timePeriod.classList = 'highlight-grow highlight';
+          if (i === startCell) {
+            timePeriod.classList = 'highlight-grow highlight highlight-start';
+          }
+          if (i === endCell) {
+            timePeriod.classList = 'highlight-grow highlight highlight-end';
+          }
+          if (startCell == endCell) {
+            timePeriod.classList =
+              'highlight-grow highlight highlight-start-end';
+          }
+          if (harvestCell !== null) {
+            if (i >= harvestCell && i <= endCell) {
+              timePeriod.style.backgroundColor = 'orangered';
+            }
+          }
+
+          tableCell.appendChild(timePeriod);
+        }
+
+        tableRow.appendChild(tableCell);
+      }
+      tableProduction.appendChild(tableRow);
+    });
+  });
+});
+
 const headerTable = `
 <tr>
 <th colspan="1">&nbsp;</th>
@@ -18,160 +140,3 @@ const headerTable = `
 <th colspan="4">Harvested</th>
 </tr>
 `;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const productionContent = document.getElementById('productionContent');
-
-  // Fetch the vegetables data
-  fetch('http://127.0.0.1:8000/api/v1/vegetable_manager/', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      Accept: 'application/json',
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((vegetablesData) => {
-      // Fetch the areas data
-      return fetch('http://127.0.0.1:8000/api/v1/area', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-          Accept: 'application/json',
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then((areasData) => {
-          areasData.forEach((area) => {
-            const areaName = document.createElement('h3');
-            areaName.classList.add('production-container__content__subtitle');
-            areaName.textContent = area.name;
-
-            const tableProduction = document.createElement('table');
-            tableProduction.classList.add('table-production');
-            tableProduction.innerHTML = headerTable;
-
-            productionContent.appendChild(areaName);
-            productionContent.appendChild(tableProduction);
-
-            const vegetablesInArea = vegetablesData.filter((vegetable) => {
-              return vegetable.area_id === area.id;
-            });
-
-            vegetablesInArea.forEach((vegetable) => {
-              const tableRow = document.createElement('tr');
-              let startMonth;
-              let startDay;
-              let startCell;
-              let endMonth;
-              let endDay;
-              let endCell;
-              let harvestCell;
-
-              const currentDate = new Date();
-
-              if (vegetable.harvest_date) {
-                const harvestDate = new Date(vegetable.harvest_date);
-                const harvestMonth = harvestDate.getMonth();
-                const harvestDay = harvestDate.getDate();
-                harvestCell = parseInt(
-                  (harvestMonth + 1) * 4 + harvestDay / 7 - 1
-                );
-              }
-
-              if (vegetable.sowing_date) {
-                const sowingDate = new Date(vegetable.sowing_date);
-                startMonth = sowingDate.getMonth();
-                startDay = sowingDate.getDate();
-              }
-
-              if (vegetable.planting_date) {
-                const plantingDate = new Date(vegetable.planting_date);
-                startMonth = plantingDate.getMonth();
-                startDay = plantingDate.getDate();
-              }
-              startCell = parseInt((startMonth + 1) * 4 + startDay / 7 - 1);
-
-              if (vegetable.remove_date) {
-                const removeDate = new Date(vegetable.remove_date);
-                endMonth = removeDate.getMonth();
-                endDay = removeDate.getDate();
-                endCell = parseInt((endMonth + 1) * 4 + endDay / 7 - 1);
-              } else {
-                endMonth = currentDate.getMonth();
-                endDay = currentDate.getDate();
-                endCell = parseInt((endMonth + 1) * 4 + endDay / 7 - 1);
-              }
-
-              for (let i = 0; i <= 51; i++) {
-                const tableCell = document.createElement('td');
-                if ((i > 0 && i < 3) || i === 51) {
-                  tableCell.setAttribute('colspan', '4');
-                }
-                if (i === 0) {
-                  tableCell.textContent = '';
-                  tableCell.style.backgroundImage = `url(./styles/assets/vegetable_icons/${vegetable.name.replace(
-                    /\s/g,
-                    ''
-                  )}.png)`;
-
-                  tableCell.classList.add('table-production__vegetable-icon');
-                }
-                if (i === 1) {
-                  tableCell.textContent = vegetable.name;
-                }
-                if (i === 2) {
-                  tableCell.textContent = vegetable.quantity;
-                }
-                if (i === 51) {
-                  tableCell.textContent = vegetable.harvest_quantity;
-                }
-
-                if (i >= startCell && i <= endCell) {
-                  const timePeriod = document.createElement('div');
-                  timePeriod.classList = 'highlight-grow highlight';
-                  if (i === startCell) {
-                    timePeriod.classList =
-                      'highlight-grow highlight highlight-start';
-                  }
-                  if (i === endCell) {
-                    timePeriod.classList =
-                      'highlight-grow highlight highlight-end';
-                  }
-                  if (startCell == endCell) {
-                    timePeriod.classList =
-                      'highlight-grow highlight highlight-start-end';
-                  }
-                  if (harvestCell !== null) {
-                    if (i >= harvestCell && i <= endCell) {
-                      timePeriod.style.backgroundColor = 'orangered';
-                    }
-                  }
-
-                  tableCell.appendChild(timePeriod);
-                }
-
-                tableRow.appendChild(tableCell);
-              }
-              tableProduction.appendChild(tableRow);
-            });
-          });
-        })
-        .catch((error) => {
-          console.error('Fetch error (areas):', error);
-        });
-    })
-    .catch((error) => {
-      console.error('Fetch error (vegetables):', error);
-    });
-});
