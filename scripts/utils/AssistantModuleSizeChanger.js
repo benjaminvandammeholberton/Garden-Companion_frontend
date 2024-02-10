d53acf6d3f259e64c8c09cfd9cdd457953134fae
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-10 08:50:09 +0100

    Improve: dashboard style for enhanced user experience

--- scripts/utils/AssistantModuleSizeChanger.js
+++ scripts/utils/AssistantModuleSizeChanger.js
@@ -1,53 +1,55 @@
 export function assistantModuleSizeChanger() {
-  const gptButton = document.getElementById('submit-button');
-  const inputElement = document.getElementById('user-input');
-  const expandElement = document.getElementById('assistant_button');
-  const chatOutput = document.querySelector('.chat-output');
-  const placeholderMessage = document.getElementById('placeholder-message');
+  const gptButton = document.getElementById("submit-button");
+  const inputElement = document.getElementById("user-input");
+  const expandElement = document.getElementById("assistant_button");
+  const chatOutput = document.querySelector(".chat-output");
+  const placeholderMessage = document.getElementById("placeholder-message");
   const assistantModule = document.querySelector(
-    '.dashbord__module--assistant'
+    ".dashbord__module--assistant"
   );
-  const forecastModule = document.querySelector('.dashbord__module--forecast');
-  const toDoModule = document.querySelector('.dashbord__module--todolist');
+  const forecastModule = document.querySelector(".dashbord__module--forecast");
+  const toDoModule = document.querySelector(".dashbord__module--todolist");
   const recommandationsModule = document.querySelector(
-    '.dashbord__module--recommandations'
+    ".dashbord__module--recommandations"
   );
-  const assistantConversationContainer = document.getElementById('chat-output');
-  const counter = document.getElementById('assistant_counter');
+  const assistantConversationContainer = document.getElementById("chat-output");
+  const counter = document.getElementById("assistant_counter");
   let isAssistantToggled = false; // Variable to track the grid state
 
   function showChatContainer() {
-    chatOutput.style.display = 'block';
+    chatOutput.style.display = "block";
   }
 
   // Function to hide the chat container
   function hideChatContainer() {}
 
   // Add a click event listener to the element
-  expandElement.addEventListener('click', (event) => {
+  expandElement.addEventListener("click", (event) => {
     // Toggle between the two grid templates
     if (isAssistantToggled) {
-      assistantModule.style.gridRow = '';
-      assistantModule.style.gridColumn = '';
-      forecastModule.style.display = 'flex';
-      toDoModule.style.display = '';
-      assistantModule.style.borderRadius = '';
-      recommandationsModule.style.display = '';
-      assistantConversationContainer.style.height = '70%';
-      counter.style.top = '18px';
-      counter.style.right = '15px';
+      assistantModule.style.gridRow = "";
+      assistantModule.style.gridColumn = "";
+      forecastModule.style.display = "flex";
+      toDoModule.style.display = "";
+      assistantModule.style.borderRadius = "";
+      recommandationsModule.style.display = "";
+      assistantConversationContainer.style.height = "100px";
+      counter.style.top = "18px";
+      counter.style.right = "15px";
+      chatOutput.scrollTop = chatOutput.scrollHeight;
       hideChatContainer();
       // placeholderMessage.style.display = 'block';
     } else {
-      counter.style.top = '18px';
-      counter.style.right = '80px';
-      assistantModule.style.gridRow = 'span 3';
-      assistantModule.style.gridColumn = 'span 2';
-      assistantModule.style.borderRadius = '5px 100px 100px 5px';
-      forecastModule.style.display = 'none';
-      recommandationsModule.style.display = 'none';
-      toDoModule.style.display = 'none';
-      assistantConversationContainer.style.height = '80%';
+      counter.style.top = "18px";
+      counter.style.right = "80px";
+      assistantModule.style.gridRow = "span 3";
+      assistantModule.style.gridColumn = "span 2";
+      assistantModule.style.borderRadius = "5px 100px 100px 5px";
+      forecastModule.style.display = "none";
+      recommandationsModule.style.display = "none";
+      toDoModule.style.display = "none";
+      assistantConversationContainer.style.height = "80%";
+      chatOutput.scrollTop = chatOutput.scrollHeight;
       showChatContainer();
       // placeholderMessage.style.display = 'none';
     }
