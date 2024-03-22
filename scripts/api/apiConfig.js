725f36d7160bd7b99d9af63e8f787a295759afb0
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-22 13:55:21 +0100

    Fix: BASE_URL variable

--- scripts/api/apiConfig.js
+++ scripts/api/apiConfig.js
@@ -1,7 +1,7 @@
 export let BASE_URL;
-
-if (window.location.hostname === "localhost" || window.location.hostname) {
-  BASE_URL = "http://localhost:8000/api/v1";
-} else {
-  BASE_URL = "https://garden-companion-api-24y73.ondigitalocean.app/api/v1";
-}
+BASE_URL = "https://garden-companion-api-24y73.ondigitalocean.app/api/v1";
+// if (window.location.hostname === "localhost" || window.location.hostname) {
+//   BASE_URL = "http://localhost:8000/api/v1";
+// } else {
+//   BASE_URL = "https://garden-companion-api-24y73.ondigitalocean.app/api/v1";
+// }
